cc0db08bfaa67a3e9f3ad603c894e2c1723f3881
ThPoi <32869786+thomaspoi@users.noreply.github.com>
2018-04-09 19:40:55 +0200

    algo finit
    algo de vie finit
    - soin
    - attaque
    - attaque spécial

--- scripts/app.js
+++ scripts/app.js
@@ -1,63 +1,68 @@
-let heal = 10
 // for characteristics player
 
 let playerName = "Thomas"
 let playerHealth = 100
-let playerStrenght = 20
+let playerStrenght = 15
 let playerRace = "Humain"
 let playerXp = 10
+let playerHeal = 10
+
 
 // for characteristics monster
 
 let monsterName = "Orc"
 let monsterHealth = 100
-let monsterStrenght = 20
+let monsterStrenght = 10
 let monsterRace = "Orc"
 let monsterXp = 10
 
-
-
 let character = {
     // Initialise le character
-    initCharacter: function (name, health, strenght, race, xp) {
+    initCharacter: function (name, health, strenght, race, xp, heal) {
         this.name = name
         this.health = health
         this.strenght = strenght
         this.race = race
         this.xp = xp
+        this.heal = heal
     },
     // Attaque un character target
     description: function () {
-      let description = this.name + " a " + this.health + " points de vie " + this.strenght + " points de force et appartient à la race " + this.race + " et a " + this.xp + " points d'xp"
+      let description = this.name + " a " + this.health + "points de vie " + this.strenght + " points de force et appartient à la race " + this.race + " et a " + this.xp + " points d'xp"
       return description
     },
     fight: function (target) {
-        if (this.health > 0) {
-            let dammage = this.strenght;
-            console.log(this.name + " attaque " + target.name + " et lui fait " + dammage + " points de dégâts");
-            target.health = target.health - dammage;
-            if (target.health > 0) {
-                console.log(target.name + " a encore " + target.health + " points de vie");
-            } else {
-                target.health = 0;
-                console.log(target.name + " est mort ! Bravo");
-            }
+      if (this.health >  0) {
+        if (target.health > 0){
+          target.health -= this.strenght
+          console.log(this.name + " attaque " + target.name)
+          console.log("Il reste " + target.health + " de point de vie à " + target.name)
         } else {
-            console.log(this.name + "est mort...");
+          console.log(target.name + " est mort !")
         }
+      } else {
+        console.log(this.name + " est mort")
+      }
+    },
+
+    special: function (target) {
+      let number = Math.random()*2
+      if (this.health >  0) {
+        if (target.health > 0){
+          target.health -= this.strenght * number
+          console.log(this.name + " attaque " + target.name)
+          console.log("Il reste " + target.health + " de point de vie à " + target.name)
+        } else {
+          console.log(target.name + " est mort !")
+        }
+      } else {
+        console.log(this.name + " est mort")
+      }
     },
 };
 
 let player = Object.create(character)
 player.initCharacter(playerName, playerHealth, playerStrenght, playerRace, playerXp)
-
-player.combat = function(monster) {
-  this.fight(monster)
-  if (monster.health === 0) {
-    console.log(this.name + " a tué " + monster.name + " et gagne " + monster.xp + " expérience")
-    this.xp += monster.xp
-  }
-}
 
 
 console.log(player.description())
@@ -68,43 +73,52 @@ monster.initCharacter(monsterName, monsterHealth, monsterStrenght, monsterRace, 
 console.log(monster.description())
 
 
-monster.fight(player);
+// Les sorts
+let pvPlayer = document.querySelector("#pvPlayer")
+let pvMonster =  document.querySelector("#pvMonster")
 
-player.combat(monster);
-
-console.log(player.description());
-
-
-
-
-
-//////// PARTIE LEO SABLONG
-let pv =  document.querySelector("#pvMonster")
-let attack = document.querySelector("#attaque")
-let howMuchPv = monsterHealth
-let damage = playerStrenght
-  attack.addEventListener(
+let attackClick = document.querySelector("#attaque")
+  attackClick.addEventListener(
     'click',
-    function attack(){
-    console.log(howMuchPv)
-    howMuchPv -= damage
-    console.log(howMuchPv)
-    pv.style.width = howMuchPv + '%'
+    function attackClick(){
+      if (monster.health > 0) {
+        player.fight(monster)
+        pvMonster.style.width = monster.health + '%'
+        monster.fight(player);
+        pvPlayer.style.width = player.health + '%'
+      } else {
+        pvMonster.style.width = 0 + '%'
+        window.alert("Bravo, le monstre est mort !")
+      }
 }
 )
 
-
-/**
-// PARTIE SOIN
-
-heal = document.querySelector("#heal")
-  heal.addEventListener(
+let specialClick = document.querySelector("#special")
+  specialClick.addEventListener(
     'click',
-    function attack(){
-   howMuchPv = howMuchPv + heal
-   pv.style.width= howMuchPv + "px"
+    function specialClick(){
+      if (monster.health > 0) {
+        player.special(monster)
+        pvMonster.style.width = monster.health + '%'
+        monster.special(player);
+        pvPlayer.style.width = player.health + '%'
+      } else {
+        pvMonster.style.width = 0 + '%'
+        window.alert("Bravo, le monstre est mort !")
+      }
 }
 )
 
-console.log(howMuchPv);
-*/
+let healClick = document.querySelector("#heal")
+  healClick.addEventListener(
+    'click',
+    function healClick(){
+      if (player.health <  100){
+        player.health += 5
+        console.log(player.health)
+        pvPlayer.style.width= player.health + "%"
+      }else {
+        window.alert("Tu es entièrement soigner")
+        }
+    }
+)
