fbdcf50fb207c13ed834c2d52c6942721490ef79
Sablong <leo.sablong@hetic.net>
2018-04-09 11:13:07 +0200

    Test Barre de Vie

--- scripts/app.js
+++ scripts/app.js
@@ -45,4 +45,65 @@ let perso1 = {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //////// PARTIE LEO SABLONG
+damage = "20%"
+pv =  document.querySelector("#pvPlayer1")
+attac = document.querySelector("#attac")
+  attac.addEventListener(
+    'click',
+    function attac(damage){
+  pv.style.width="damage"
+}
+)
