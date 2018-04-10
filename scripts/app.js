e15bb9f49b8d8aefbc43ebbe9bd4bb6b9f870d35
ThPoi <32869786+thomaspoi@users.noreply.github.com>
2018-04-10 20:44:53 +0200

    master

--- scripts/app.js
+++ scripts/app.js
@@ -31,19 +31,88 @@ let character = {
       let description = this.name + " a " + this.health + "points de vie " + this.strenght + " points de force et appartient à la race " + this.race + " et a " + this.xp + " points d'xp"
       return description
     },
-    fight: function (target) {
+    fightPlayer: function (target) {
+      let text = "COMBAT"
+      let yo = ""
 
-      if (this.health >  0) {
-        if (target.health > 0){
-          target.health -= this.strenght
-          console.log(this.name + " attaque " + target.name)
-          console.log("Il reste " + target.health + " de point de vie à " + target.name)
+      let action = document.createElement("p")
+      action.textContent = text
+
+      let dead = document.createElement("p")
+      dead.textContent = "Le monstre est déjà mort !"
+
+      document.getElementById("textWrite").appendChild(action)
+      if (target.health > 0) {
+        // CHOICE USER
+        let choicePlayer = window.prompt("pierre, feuille, ciseaux !")
+        choicePlayer =choicePlayer.toLowerCase()
+        let affichageChoicePlayer= document.createElement("p")
+        affichageChoicePlayer.textContent = "Vous avez choisi " + choicePlayer
+        document.getElementById("textWrite").appendChild(affichageChoicePlayer)
+
+        // CHOICE TARGET
+        let choiceTarget = Math.random()
+        if (choiceTarget < 0.34){
+          choiceTarget = "pierre"
+        } else if (choiceTarget <=0.66) {
+          choiceTarget = "feuille"
         } else {
-          console.log(target.name + " est mort !")
+          choiceTarget = "ciseaux"
         }
+        console.log("Le monstre a choisit " + choiceTarget)
+
+        // COMPARAISON
+        let affichageChoiceTarget = document.createElement("p")
+        affichageChoiceTarget.textContent = target.name + " a choisit " + choiceTarget
+        document.getElementById("textWrite").appendChild(affichageChoiceTarget)
+
+        let affichageResult = document.createElement("p")
+
+        let winnerFight
+        let looserFight
+        if (choicePlayer === choiceTarget) {
+          affichageResult.textContent = "Egalité, personne n'a attaqué !"
+          document.getElementById("textWrite").appendChild(affichageResult)
+          return false
+        } else if (choicePlayer === "pierre" && choiceTarget === "ciseaux") {
+          winnerFight = this.name
+          looserFight = target.name
+          target.health -= this.strenght
+          attacPlayer()
+        } else if (choicePlayer === "pierre" && choiceTarget === "feuille") {
+          winnerFight = target.name
+          looserFight = this.name
+          this.health -= target.strenght
+          attacMonster()
+        } else if (choicePlayer === "ciseaux" && choiceTarget === "pierre") {
+          winnerFight = target.name
+          looserFight = this.name
+          this.health -= target.strenght
+          attacPlayer()
+        } else if (choicePlayer === "ciseaux" && choiceTarget === "feuille") {
+          winnerFight = this.name
+          looserFight = target.name
+          target.health -= this.Strenght
+          attacMonster()
+        } else if (choicePlayer === "feuille" && choiceTarget === "ciseaux") {
+          winnerFight = target.name
+          looserFight = this.name
+          this.health -= target.strenght
+          attacMonster()
+        } else if (choicePlayer === "feuille" && choiceTarget === "pierre") {
+          winnerFight = this.name
+          looserFight = target.name
+          target.health -= this.strenght
+          attacPlayer()
+        }
+        affichageResult.textContent = winnerFight + " attaque " + looserFight
+        document.getElementById("textWrite").appendChild(affichageResult)
+
+
       } else {
-        console.log(this.name + " est mort")
+        document.getElementById("textWrite").appendChild(dead)
       }
+
     },
 
     special: function (target) {
@@ -81,34 +150,67 @@ monster.initCharacter(monsterName, monsterHealth, monsterStrenght, monsterRace, 
 console.log(monster.description())
 
 
-/// animation
+/// animation attac
 
-function animationAttack() {
 
-}
 
-//////// PARTIE LEO SABLONG
+//////// SORTS
 let pvPlayer = document.querySelector("#pvPlayer")
 let pvMonster =  document.querySelector("#pvMonster")
 
+let playerCharacter = document.getElementById("player")
+let monsterCharacter = document.getElementById("monster")
 
+// function utiliser pour animation player
+function readyPlayer (){
+  playerCharacter.setAttribute("src", "images/animation_character/Anduin_ready.gif")
+}
+function noPlayer (){
+  playerCharacter.setAttribute("src", "images/static_character/tombe.png")
+
+}
+function attacPlayer () {
+  playerCharacter.setAttribute("src", "images/animation_character/Anduin_attaque.gif")
+  setTimeout(readyPlayer, 1000);
+}
+function specialPlayer () {
+  playerCharacter.setAttribute("src", "images/animation_character/Anduin_as.gif")
+  setTimeout(readyPlayer, 1000);
+}
+function deadPlayer () {
+  playerCharacter.setAttribute("src", "images/animation_character/Anduin_death.gif")
+  setTimeout(noPlayer, 2000);
+}
+// function utiliser pour animation monster
+function readyMonster (){
+  monsterCharacter.setAttribute("src", "images/animation_character/Ogre_ready.gif")
+}
+function noMonster (){
+  monsterCharacter.setAttribute("src", "images/static_character/tombe.png")
+}
+function attacMonster () {
+  monsterCharacter.setAttribute("src", "images/animation_character/Ogre_attaque.gif")
+  setTimeout(readyMonster, 1000);
+}
+function deadMonster () {
+  monsterCharacter.setAttribute("src", "images/animation_character/Ogre_death.gif")
+  setTimeout(noMonster, 2000);
+}
 let attackClick = document.querySelector("#attaque")
   attackClick.addEventListener(
     'click',
     function attackClick(){
-      player.fight(monster)
-      monster.fight(player)
-      document.getElementById("player").src = "../images/animation_character/Anduin_attaque_1.gif"
-      document.getElementById("monster").src = "../images/animation_character/Ogre_attaque.gif"
+      player.fightPlayer(monster)
       if (monster.health > 0 && player.health > 0) {
         pvMonster.style.width = monster.health + '%'
         pvPlayer.style.width = player.health + '%'
       } else if (monster.health > 0 && player.health <= 0){
         pvPlayer.style.width = 0 + '%'
-        window.alert("Vous êtes mort")
+        deadPlayer()
+        console.log("Vous êtes mort")
       } else {
         pvMonster.style.width = 0 + '%'
-        window.alert("Bravo, le monstre est mort !")
+        console.log("Bravo, le monstre est mort !")
       }
 }
 );
@@ -118,22 +220,21 @@ let specialClick = document.querySelector("#special")
     'click',
     function specialClick(){
       player.special(monster)
-      monster.fight(player)
-      document.getElementById("player").src = "../images/animation_character/Anduin_as.gif"
-      document.getElementById("monster").src = "../images/animation_character/Ogre_attaque.gif"
+
       if (monster.health > 0 && player.health > 0) {
         pvPlayer.style.width = player.health + '%'
         pvMonster.style.width = monster.health + '%'
       } else if (monster.health > 0 && player.health <= 0){
         pvPlayer.style.width = 0 + '%'
-        window.alert("Vous êtes mort")
+
+        console.log("Vous êtes mort")
       } else {
         pvMonster.style.width = 0 + '%'
-        window.alert("Bravo, le monstre est mort !")
+        console.log("Bravo, le monstre est mort !")
       }
 }
 );
-
+/*
 let healClick = document.querySelector("#heal")
 let time = 0
   healClick.addEventListener(
@@ -144,9 +245,10 @@ let time = 0
         pvPlayer.style.width = player.health + "%"
         time +=1
       } else if (time >2){
-        window.alert("Vous ne pouvez plus vous soignez")
+        console.log("Vous ne pouvez plus vous soignez")
       } else {
-        window.alert("Tu as déjà des points de vie au maximun !")
+        console.log("Tu as déjà des points de vie au maximun !")
       }
     }
 )
+*/
