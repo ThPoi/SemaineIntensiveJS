1fe1c1996e4b7824a95c12bfc624288b489b422d
Sablong <leo.sablong@hetic.net>
2018-04-10 14:18:26 +0200

    ajout carte

---
+++ carte/scripts/app.js
@@ -0,0 +1,42 @@
+console.log("hello world")
+
+window.addEventListener('load', () => {
+
+  var elem = document.querySelector(".myAnimation");
+  var pos = 0
+  var posTop = 300;
+  var posLeft = 350;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos+ posTop + 'px';
+      elem.style.left = pos + posLeft + 'px';
+    }
+  }
+})
+
+setTimeout(function(){
+let button = document.querySelector('button')
+button.classList.remove('none')
+button.classList.add('End1')
+}, 4000)
+
+buton = document.querySelector("buton")
+function myMove2() {
+  var elem = document.querySelector(".myAnimation");
+  var pos = 0
+  var posTop = 650;
+  var posLeft = 700;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 400) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.left = pos + posLeft + 'px';
+    }
+  }
+}
