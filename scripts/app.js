1d05470089148288d928a0718c746f674489105f
ThPoi <32869786+thomaspoi@users.noreply.github.com>
2018-04-11 21:02:26 +0200

    test

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 // variable pour vérifier bon fonctionne script
 let animationTrue = 0
 let deadVerif = 0
+let turnPerMonster = 0
 // for characteristics player
 
 let playerName = "Anduin"
@@ -34,11 +35,11 @@ let character = {
     },
     fight: function (target) {
       let combatText = "COMBAT"
-
       let combat = document.createElement("p")
       combat.textContent = combatText
       document.getElementById("textWrite").appendChild(combat)
       scrollHeight ()
+      console.log(this.id)
 
       let actionText = ""
       let action = document.createElement("p")
@@ -52,7 +53,7 @@ let character = {
         attacCharacter(this.id)
 
       } else if (number <=9) {
-        target.health -= this.strenght * 1.5
+        target.health -= this.strenght * 1.3
         actionText = this.name + " fait une attaque critique sur " + target.name + " !"
         attacCharacter(this.id)
 
@@ -62,6 +63,7 @@ let character = {
       action.textContent = actionText
       document.getElementById("textWrite").appendChild(action)
       scrollHeight ()
+      consoleLogCarac()
 
     },
     care: function(target) {
@@ -70,7 +72,7 @@ let character = {
       action.textContent = action
       let number = Math.random()*2
       if (number<1) {
-        this.health += target.strenght*2
+        this.health *= target.strenght * 1
         if (this.health > 100) {
           this.health = 100
         }
@@ -81,10 +83,10 @@ let character = {
       action.textContent = actionText
       document.getElementById("textWrite").appendChild(action)
       scrollHeight ()
+      consoleLogCarac()
     },
     special: function (target) {
       let combatText = "COMBAT"
-
       let combat = document.createElement("p")
       combat.textContent = combatText
       document.getElementById("textWrite").appendChild(combat)
@@ -96,21 +98,109 @@ let character = {
 
       let number = Math.random()*10
 
-      if (number <=6) {
-        actionText = this.name + " ne réussit pas son attaque spéciale sur "+ target.name
+      if (number <=2) {
+        this.health -= this.strenght * 1.5
+        actionText = "L'attaque spéciale de " + this.name + "se retourne contre lui !"
 
-      } else if (number <=7) {
-        target.health -= this.strenght * 4
+      } else if (number <=5) {
+        target.health -= this.strenght * 1.5
         actionText = this.name + " fait une attaque spéciale sur " + target.name + " !"
         specialCharacter(this.id)
 
       } else {
-        this.health -= this.strenght * 2
-        actionText = "L'attaque spéciale de " + this.name + "se retourne contre lui !"
+        actionText = this.name + " ne réussit pas son attaque spéciale sur "+ target.name
+
       }
       action.textContent = actionText
       document.getElementById("textWrite").appendChild(action)
       scrollHeight ()
+      consoleLogCarac()
+
+    },
+    defense: function(target) {
+      let combatText = "COMBAT"
+
+      let combat = document.createElement("p")
+      combat.textContent = combatText
+      document.getElementById("textWrite").appendChild(combat)
+      scrollHeight ()
+
+      let actionText = ""
+      let action = document.createElement("p")
+      action.textContent = action
+      let number = Math.random()*2
+      if (number<1.5) {
+        this.health = this.health
+        actionText = this.name + " a réussi son blocage"
+      } else if (number <=2){
+        actionText = this.name + " a echoué son blocage"
+      }
+      action.textContent = actionText
+      document.getElementById("textWrite").appendChild(action)
+      scrollHeight ()
+      consoleLogCarac()
+    },
+    increase: function(target) {
+      let combatText = "COMBAT"
+      let combat = document.createElement("p")
+      combat.textContent = combatText
+      document.getElementById("textWrite").appendChild(combat)
+      scrollHeight ()
+
+      let actionText = ""
+      let action = document.createElement("p")
+      action.textContent = action
+
+      let number = Math.random()*10
+
+      if (number <=5) {
+        console.log("5")
+        this.strenght += this.strenght * 0.2
+        actionText = this.name + " a réussi son augmentation de puissance"
+
+
+      } else if (number <=6) {
+        this.strenght += this.strenght * 0.5
+        actionText = this.name + " a fait une augmentation critique de puissance"
+
+
+      } else {
+        actionText = this.name + " a raté son augmentation de puissance"
+      }
+      action.textContent = actionText
+      document.getElementById("textWrite").appendChild(action)
+      scrollHeight ()
+      consoleLogCarac()
+
+    },
+    decrease: function(target) {
+      let combatText = "COMBAT"
+      let combat = document.createElement("p")
+      combat.textContent = combatText
+      document.getElementById("textWrite").appendChild(combat)
+      scrollHeight ()
+
+      let actionText = ""
+      let action = document.createElement("p")
+      action.textContent = action
+
+      let number = Math.random()*10
+
+      if (number <=5) {
+        target.strenght -= target.strenght * 0.1
+        actionText = this.name + " a réussi a enlever de la puissance a " + target.name
+
+      } else if (number <=6) {
+        target.strenght -= target.strenght * 0.2
+        actionText = this.name + " a réussit à enlever beaucoup de puissance a " + target.name
+
+      } else {
+        actionText = this.name + " n'a pas réussit à enlever de la puissance à " + target.name
+      }
+      action.textContent = actionText
+      document.getElementById("textWrite").appendChild(action)
+      scrollHeight ()
+      consoleLogCarac()
 
     },
 };
@@ -199,24 +289,34 @@ let pvMonster =  document.querySelector("#pvMonster")
 
 let playerCharacter = document.getElementById("player")
 let monsterCharacter = document.getElementById("monster")
-let attackClick = document.querySelector("#attaque")
 
+// RAPPEL
+
+let turnMonster = 0
+
+let attackClick = document.querySelector("#attaque")
   attackClick.addEventListener(
     'click',
     function attackClick(){
-      if (animationTrue === 1 && deadVerif === 0) {
-        setTimeout(function() {
-          readyCharacter(this.id)
-        }, 1000)
-
-      } else if (animationTrue === 0 && deadVerif === 0){
-        player.fight(monster)
-      } else if (deadVerif === 1) {
-        deadStop()
+      console.log(turnMonster)
+      if (turnMonster === 0) {
+        if (animationTrue === 1 && deadVerif === 0) {
+          setTimeout(function() {
+            readyCharacter(this.id)
+          }, 1000)
+        } else if (animationTrue === 0 && deadVerif === 0){
+          player.fight(monster)
+        } else if (deadVerif === 1) {
+          deadStop()
+        } else {
+          console.log("erreur attack")
+        }
+        turnMonster = 1
+        managementHealth ()
+        turnForMonster ()
       } else {
-        console.log("erreur")
+        waitMonster()
       }
-      managementHealth ()
 }
 );
 
@@ -231,12 +331,14 @@ healClick.addEventListener(
       scrollHeight ()
     } else if (player.health < 100) {
       player.care(monster)
+    } else {
+      console.log("erreur heal")
     }
     managementHealth ()
 }
 );
-let specialClick = document.querySelector("#special")
 
+let specialClick = document.querySelector("#special")
 specialClick.addEventListener(
   'click',
   function specialClick(){
@@ -250,7 +352,66 @@ specialClick.addEventListener(
     } else if (deadVerif === 1) {
       deadStop()
     } else {
-      console.log("erreur")
+      console.log("erreur special")
+    }
+    managementHealth ()
+}
+);
+
+let defenseClick = document.querySelector("#defense")
+defenseClick.addEventListener(
+  'click',
+  function specialClick(){
+    if (animationTrue === 1 && deadVerif === 0) {
+      setTimeout(function() {
+        defenseCharacter(this.id)
+      }, 1000)
+
+    } else if (animationTrue === 0 && deadVerif === 0){
+      player.defense(monster)
+    } else if (deadVerif === 1) {
+      deadStop()
+    } else {
+      console.log("erreur defense")
+    }
+    managementHealth ()
+}
+);
+
+let increaseClick = document.querySelector("#increase")
+increaseClick.addEventListener(
+  'click',
+  function increaseClick(){
+    if (animationTrue === 1 && deadVerif === 0) {
+      setTimeout(function() {
+        increaseCharacter(this.id)
+      }, 1000)
+
+    } else if (animationTrue === 0 && deadVerif === 0){
+      player.increase(monster)
+    } else if (deadVerif === 1) {
+      deadStop()
+    } else {
+      console.log("erreur increase")
+    }
+    managementHealth ()
+}
+);
+let decreaseClick = document.querySelector("#decrease")
+decreaseClick.addEventListener(
+  'click',
+  function decreaseClick(){
+    if (animationTrue === 1 && deadVerif === 0) {
+      setTimeout(function() {
+        decreaseCharacter(this.id)
+      }, 1000)
+
+    } else if (animationTrue === 0 && deadVerif === 0){
+      player.decrease(monster)
+    } else if (deadVerif === 1) {
+      deadStop()
+    } else {
+      console.log("erreur decrase")
     }
     managementHealth ()
 }
@@ -260,29 +421,34 @@ specialClick.addEventListener(
 
 
 
+
+
 // function divers
 // gestion vie
 function managementHealth () {
-  if (monster.health > 0 && player.health > 0) {
-    pvMonster.style.width = monster.health + '%'
-    pvPlayer.style.width = player.health + '%'
-  } else if (monster.health > 0 && player.health <= 0){
-    pvPlayer.style.width = 0 + '%'
-    deadCharacter(player.id)
-    let deadText = "Vous êtes mort !"
-    let dead = document.createElement("p")
-    dead.textContent = deadText
-    document.getElementById("textWrite").appendChild(dead)
-    console.log("Player mort")
-  } else if (deadVerif === 0) {
-    deadVerif = 1
-    pvMonster.style.width = 0 + '%'
-    deadCharacter(monster.id)
-  } else if(monster.health<=0){
-    console.log("Monstre mort")
-  } else {
-    console.log("Error life")
-  }
+  setTimeout(function(){
+    if (monster.health > 0 && player.health > 0) {
+      pvMonster.style.width = monster.health + '%'
+      pvPlayer.style.width = player.health + '%'
+    } else if (monster.health > 0 && player.health <= 0){
+      pvPlayer.style.width = 0 + '%'
+      deadCharacter(player.id)
+      let deadText = "Vous êtes mort !"
+      let dead = document.createElement("p")
+      dead.textContent = deadText
+      document.getElementById("textWrite").appendChild(dead)
+      console.log("Player mort")
+    } else if (deadVerif === 0) {
+      deadVerif = 1
+      pvMonster.style.width = 0 + '%'
+      deadCharacter(monster.id)
+    } else if(monster.health<=0){
+      console.log("Monstre mort")
+    } else {
+      console.log("Error life")
+    }
+  }, 1000);
+
 }
 //gestion mort
 function deadStop() {
@@ -292,8 +458,60 @@ function deadStop() {
   document.getElementById("textWrite").appendChild(dead)
   scrollHeight ()
 }
-// fonction hasard
-// trois tiers
+// attendre monster
+function waitMonster() {
+  let waitMonsterText = "Attendez que le monstre finisse de jouer ! Soyez galant !"
+  let waitMonster = document.createElement("p")
+  waitMonster.textContent = waitMonsterText
+  document.getElementById("textWrite").appendChild(waitMonster)
+  scrollHeight ()
+}
+// possibilité jusqu'a 10
+function threeChoice () {
+  let number = Math.floor(Math.random()*10+1)
+  return number
+}
+//  BOT monster
+function turnForMonster () {
+  setTimeout(
+    function turn(){
+      let choice
+      if (monster.health <=100) {
+        choice = threeChoice ()
+        if (monster.health <= 100) {
+          monster.fight(player)
+        } else if (monster.health <= 20){
+          monster.special(player)
+        } else if (monster.health <= 50){
+          monster.special(player)
+        } else if (monster.health <= 90){
+          monster.special(player)
+        } else if (monster.health <= 100){
+          monster.special(player)
+        }
+      } else if(){
+        console.log("Erreur, monster n'a plus de vie")
+      } else {
+        console.log("Erreur, monster n'a plus de vie")
+      }
+      managementHealth ()
+      console.log("turn monster")
+
+  }, 1000);
+
+}
 
 
-// pile face
+
+// CONSOLE LOG character
+function consoleLogCarac (){
+  console.log("CARACTERISTIQUES")
+  console.log(monster.name)
+  console.log(monster.strenght)
+  console.log(monster.health)
+  console.log(player.name)
+  console.log(player.strenght)
+  console.log(player.health)
+}
+
+consoleLogCarac()
